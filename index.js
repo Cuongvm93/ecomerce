125385d914d2f916a0e9c8e6e75ccc57dd36f79e
cuongvm93 <cuongvu6693@gmail.com>
2022-10-18 17:34:38 +0700

    commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let data=[
     {   id: "id0",
         img:"./adidas/adidasID1.jpg",
+        img1:"./adidas/Adidas ID0-2.JPG",
+        img2:"./adidas/Adidas ID0-2.JPG",
+        img3:"./adidas/adidas ID0-3.JPG",
         title:"CAMPUS 80S SOUTH PARK TOWELIE",
-        des:"Inspired by the original AJ1, the Air Jordan 1 Mid offers fans a chance to follow in MJ's footsteps. Fresh color trims the clean, classic materials, imbuing modernity into a classic design.",
+        des:"Don't forget to bring a towel. Hard to do when you've got Towelie on your feet. In the form of the famed adidas Campus shoes, that is. Plus it says it right there on the tongue. Step into the sun and the eyes turn red. A small hook-and-loop pocket on the inside of the tongue lets you store whatever it is you might be able to fit in there. For now it's a Towelie keychain.",
         price:80,
         cate:"adidas"
 
@@ -10,47 +13,65 @@ let data=[
     {
         id:"id1",
         img:"./adidas/adidasId2.jpg",
+        img1:"./adidas/Adidas ID1-1.JPG",
+        img2:"./adidas/Adidas ID1-2.JPG",
+        img3:"",
         title:"Sam smith Shoe", 
-        des:"Inspired by the original AJ1",
+        des:"Timeless appeal. Effortless style. Everyday versatility. For over 50 years and counting, adidas Stan Smith Shoes have continued to hold their place as an icon. This pair shows off a fresh redesign as part of adidas' commitment to use only recycled polyester by 2024. With a vegan upper and an outsole made from rubber waste, they still have iconic style, they're just made with the planet in mind.",
         price:120,
         cate:"adidas"
     },
     {
         id:"id2",
         img:"./adidas/adidasID3.jpg",
+        img1:"./adidas/adidas ID2-1.JPG",
+        img2:"./adidas/Adidas ID2-2.JPG",
+        img3:"./adidas/Adidas ID2-3.JPG",
         title:"Ultimashow Shoes",
-        des:"Inspired by the original id2",
+        des:"Eliminate the noise. As you push yourself, all you can think about is your movement and your breath. And that you're doing it. The rest falls away. Move fast. Move slow. Just move. And do it in these adidas running shoes. They're lightweight, cushioned and ready to roll.",
         price:85,
         cate:"adidas"
     },
     {
         id:"id3",
         img:"./adidas/AdidasID4.JPG",
+        img1:"./adidas/Adidas ID3-1.JPG",
+        img2:"./adidas/Adidas ID3-2.JPG",
+        img3:"./adidas/Adidas ID3-3.JPG",
         title:"Manchester United 22/23 Home Jersey",
-        des:"Inspired by the original id3",
+        des:"Khi dựng lên cũng như khi bẻ xuống, chiếc cổ áo polo khiêm tốn luôn đóng vai trò nổi bật trong rất nhiều những khoảnh khắc huy hoàng nhất của Manchester United. Tái xuất trên chiếc áo đấu bóng đá adidas dành cho thiếu niên này, thiết kế cổ áo ấy kết hợp cùng huy hiệu hình chiếc khiên và họa tiết sọc nhỏ tạo nên diện mạo bắt mắt. Công nghệ AEROREADY thấm hút ẩm và các mảng phối lưới mang đến cảm giác thoải mái cho các fan hâm mộ cuồng nhiệt.",
         price:170,
         cate:"adidas"
     },
     {
         id:"id4",
         img:"./adidas/AdidasID5.Jpg",
+        img1:"./adidas/Adidas ID4-1.JPG",
+        img2:"./adidas/Adidas ID4-2.JPG",
+        img3:"./adidas/Adidas ID4-3.JPG",
         title:"Forum Low CL",
-        des:"Inspired by the original id4",
+        des:"When the adidas Forum debuted in '84, it quickly dominated the basketball scene — and for good reason. It's forward-thinking tech raised the bar on the hardwood, and its swagger made waves off it. Back in a low top iteration with premium leather and all the classic details, these shoes honour heritage while keeping your style fresh. We even removed the top strap to let that crisscross ankle and tongue label really shine.",
         price:75,
         cate:"adidas"
     },
     {
         id:"id5",
         img:"./Nike/id1.jpg",
+        img1:"./Nike/Nike ID0-1.JPG",
+        img2:"./Nike/Nike  ID0-2.JPG",
+        img3:"./Nike/Nike ID0-3.JPG",
         title:"Air Jordan 1 Mid",
-        des:"Inspired by the original id5",
+        des:"Inspired by the original AJ1, the Air Jordan 1 Mid offers fans a chance to follow in MJ's footsteps. Fresh color trims the clean, classic materials, imbuing modernity into a classic design.",
         price:125,
         cate:"nike"
     },
     {
         id:"id6",
         img:"./Nike/id2.jpg",
-        title:"Nike Air Force",
+        img1:"./Nike/Nike ID6-1.JPG",
+        img2:"./Nike/Nike ID6-2.JPG",
+        img3:"./Nike/Nike ID6-3.JPG",
+        title:"./Nike Air Force",
         des:"Inspired by the original id6",
         price:130,
         cate:"nike"
@@ -58,24 +79,33 @@ let data=[
     {
         id:"id7",
         img:"./Nike/id3.jpg",
-        title:"Nike Dunk High Retro",
-        des:"Inspired by the original id7",
+        img1:"./Nike/NIke ID7-1.JPG",
+        img2:"./Nike/NIke ID7-2.JPG",
+        img3:"./Nike/Nike ID7-3.JPG",
+        title:"Air Jordan",
+        des:"Created for the hardwood but taken to the streets, the '80s b-ball icon returns with perfectly sheened overlays and original university colors. With its classic hoops design, the Nike Dunk High Retro channels '80s vintage back onto the streets while its padded, high-top collar adds an old-school look rooted in comfort.",
         price:125,
         cate:"nike"
     },
     {
         id:"id8",
         img:"./Nike/nike4.jpg",
+        img1:"./Nike/Nike ID8-1.JPG",
+        img2:"./Nike/Nike ID8-2.JPG",
+        img3:"./Nike/Nike ID8-3.JPG",
         title:"Men wear leg",
-        des:"Inspired by the original id8", 
+        des:"Made with soft fleece for lightweight comfort, these pants are ideal for casual, everyday wear. The relaxed fit is designed to give you some room through the hips, seat and thighs for laid back style and easy movement. Gusseted leg paneling gives you extra room so the pants move with you, not against you.", 
         price:135,
         cate:"nike"
     },
     {
         id:"id9",
         img:"./Nike/nike5.jpg",
+        img1:"./Nike/Nike ID9-1.JPG",
+        img2:"./Nike/NIke ID9-2.JPG",
+        img3:"./Nike/Nike ID9-3.JPG",
         title:"Nike SB Force 58",
-        des:"Inspired by the original id9",
+        des:"The latest and greatest innovation to hit the streets, the Nike SB Force 58 gives you the durability of a cupsole with the flexibility of a vulcanized shoe. Made from canvas and suede and finished with perforations, the whole look is infused with heritage basketball DNA.",
         price:80,
         cate:"nike",
         
@@ -83,32 +113,44 @@ let data=[
     {
         id:"id10",
         img:"./dior/dior3.jpg",
+        img1:"./dior/DIOR 10-1.jpg",
+        img2:"./dior/Dior 10-2.jpg",
+        img3:"./dior/dior 10-3.JPG",
         title:"Large DiorAlps Dior Book Tote",
-        des:"Inspired by the original id10",
+        des:"Introduced by Maria Grazia Chiuri, Creative Director of Christian Dior, the Dior Book Tote has become a staple of the Dior aesthetic. The design is part of the DiorAlps capsule and is fully embroidered with Mr. Dior’s lucky star in blue three-tone, and is further adorned</br>with the 'CHRISTIAN DIOR PARIS' signature on the front. The large tote exemplifies the House's signature savoir-faire and will pair well with other DiorAlps creations, and may be carried by hand or worn over the shoulder.",
         price:3788,
         cate:"dior"
     },
     {
         id:"id11",
         img:"./dior/dior4.jpg",
+        img1:"./dior/dior f1 1-1.jpg",
+        img2:"./dior/dior f1 1-2.jpg",
+        img3:"./dior/dior f1 1-3.jpg",
         title:"Large Dior Book Tote",
-        des:"Inspired by the original id111",
+        des:"Introduced by Maria Grazia Chiuri, Dior's Creative Director, the Dior Book Tote has become a staple of the Dior aesthetic. Designed to hold all the daily essentials, the style is fully embroidered with an ecru multicolor Dior Jardin d'Hiver motif, showcasing a poetic and exotic interpretation of Mr. Dior's wall tapestries. Adorned with the 'CHRISTIAN DIOR PARIS' signature on the front, the large tote exemplifies the House's signature savoir-faire and may be carried by hand or worn over the shoulder.",
         price:4095,
         cate:"dior"
     },
     {
         id:"id12",
         img:"./dior/dior1.jpg",
+        img1:"./dior/Dior ID12-1.JPG",
+        img2:"./dior/DIOR ID12-2.JPG",
+        img3:"./dior/Dior ID12-3.JPG",
         title:"Mini dior Bag",
-        des:"Inspired by the original id12",
+        des:"The Dior Book Tote, as imagined by Maria Grazia Chiuri, has become an essential item of the Dior spirit. Playing on House codes, the silhouette is fully embroidered with the black micro Houndstooth motif, an iconic House pattern. A prime example of the House's savoir-faire, the miniature bag may be carried by hand and can be paired with other Houndstooth creations from the collection.",
         price:4200,
         cate:"dior"
     },
     {
         id:"id13",
         img:"./dior/dior2.jpg",
+        img1:"./dior/DIor ID13-1.JPG",
+        img2:"./dior/DIOR ID13-2.JPG",
+        img3:"./dior/DIOR ID13-3.JPG",
         title:"Medium Dior book Tole",
-        des:"Inspired by the original id13",
+        des:"The Dior Book Tote is an original creation introduced by Maria Grazia Chiuri, Dior's Creative Director, and has become a staple of the Dior aesthetic. Designed to hold all the daily essentials, it is fully embroidered with the metallic blue Dior Brocart motif, offering a new take on House codes. Adorned with the 'CHRISTIAN DIOR PARIS' signature on the front, the medium tote exemplifies the House's signature savoir-faire and may be carried by hand or worn over the shoulder.",
         price:4150,
         cate:"dior"
     }   
